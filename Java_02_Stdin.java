b9f0000874e52f78b7a950f7bad81b2f7929dd3a
A.Chanikya <chanikya955@gmail.com>
2025-07-10 21:29:56 +0530

    Added the comment lines

--- Java_02_Stdin.java
+++ Java_02_Stdin.java
@@ -1,5 +1,4 @@
-import java.util.*
-;
+import java.util.*;
 public class Java_02_Stdin {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -7,10 +6,9 @@ public class Java_02_Stdin {
         int b = scan.nextInt();
         int c = scan.nextInt();
 
+        // print the output
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        
     }
-    
 }
